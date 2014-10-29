e6585d8803f1339dd691659690c47ebb0378f7ca
blueelwang <blueelwang@gmail.com>
2014-10-29 14:27:06 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+if ( ! defined('GETPIC')) exit('No direct script access allowed');
+
+define('HOST','xxxx:3307');
+define('USER','xxx');
+define('PASSWD','xxx');
+define('DATABASE','image');
+
+define('TMP_PATH','/tmp/image_cache/');
